b447235ab00969f16a0af3972a67f0255bdaec64
Wieland,S.R. (Stijn) <s.r.wieland@students.uu.nl>
2022-03-24 14:11:53 +0100

    Reset

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from dataloaders import dataloaders as dl
-
-# Running the following 2 lines will read in the dataset that is passed as argument
-# and will print info about the dataframe
-df = dl.dataloader('lfm_360k_usersha1-artmbid-artname-plays')
-print(f"{df[list(df.keys())[0]][:2]}")
-
-#PS C:\Users\sanaz\OneDrive\Documents\GitHub\fair-rec-kit-lib> python .\main.py
-#for testing instead of lfm_360k we can put ml_100k_u . One hint. if we write the name of the file from config.ini compeltely then it takes less memory
+print("Hello world")

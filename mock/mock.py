ef56a1b692989f5963e448f76187dd5531a3984c
Wieland,S.R. (Stijn) <s.r.wieland@students.uu.nl>
2022-03-24 14:43:33 +0100

    Lint2

--- mock/mock.py
+++ mock/mock.py
@@ -2,4 +2,5 @@
 Mock docstring
 """
 def mock_func(test_var):
+    """Mock doc"""
     return test_var * 2
--- test/test_mock.py
+++ test/test_mock.py
@@ -6,8 +6,9 @@ import unittest
 from mock import mock
 
 class TestMock(unittest.TestCase):
-
+    """Mock doc"""
     def test_mock(self):
+        """Mock doc"""
         self.assertEqual(mock.mock_func(2), 4)
 
 if __name__ == '__main__':
